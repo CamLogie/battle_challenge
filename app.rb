ab2563f424668d4b3c01541b19513ad7d512207a
Cameron Logie <cameronlogie24@gmail.com>
2020-10-13 17:04:08 +0100

    cat form view file made

--- app.rb
+++ app.rb
@@ -9,13 +9,17 @@ get '/secret' do
   "If you are on this page, and reading this, you should not be"
 end
 
-get '/name_cat' do
+post '/name_cat' do
   p params
-  @name_selector = params[:name]
+  @name = params[:name]
   erb(:index)
 end
 
 get '/random_cat' do
-  @name_selector = ["Cameron", "Adrian", "AT", "CL"].sample
+  @name = ["Cameron", "Adrian", "AT", "CL"].sample
   erb(:index)
+end
+
+get '/cat_name' do
+  erb(:cat_naming_form)
 end
